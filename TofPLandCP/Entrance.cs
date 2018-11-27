38822426c5304c69a6ea35ed1859aafcf8671bd9
Sergey <sborisenko.infernet@gmail.com>
2018-11-27 22:37:19 +0300

    Финальный коммит

---
+++ TofPLandCP/Entrance.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TofPLandCP
+{
+    /// <summary>
+    /// Лексема на входе по таблице 0
+    /// </summary>
+    enum Entrance_0
+    {
+        Константа=0,
+        Идентификатор,
+        Метка,
+        СкобкаО,
+        СкобкаЗ,
+        Запятая,
+        ENDL,
+        IF,
+        GOTO,
+        CALL,
+        RETURN,
+        END,
+        Равно,
+        TYPE,
+        STOP,
+        PROGRAM,
+        SUMROUTINE,
+        FUNCTION,
+        PARAMETER,
+        ОП3,
+        ОП4,
+        ОП5,
+        ОП6
+    }
+    /// <summary>
+    /// Лексема на входе по таблице 1
+    /// </summary>
+    enum Entrance_1
+    {
+        СкобкаЗ=0,
+        СкобкаО,
+        Константа,
+        Идентификатор,
+        OTHER
+    }
+    /// <summary>
+    /// Лексема на входе по таблице 2
+    /// </summary>
+    enum Entrance_2
+    {
+        СкобкаЗ=0,
+        OTHER
+    }
+    /// <summary>
+    /// Лексема на входе по таблице 3
+    /// </summary>
+    enum Entrance_3
+    {
+        СкобкаО=0,
+        OTHER
+    }
+}
---
+++ TofPLandCP/State.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TofPLandCP
+{
+    /// <summary>
+    /// Состояние в стеке по таблице 0
+    /// </summary>
+    enum State_0
+    {
+        EMPTY=0,
+        Скобка,
+        iA,
+        IF,
+        TiIF,
+        iProc,
+        iDef,
+        iTYPE,
+        RETURN,
+        STOP,
+        Равно,
+        GOTO,
+        CALL,
+        ОП3,
+        ОП4,
+        ОП5,
+        ОП6,
+    }
+    /// <summary>
+    /// Состояние в стеке по таблице 1
+    /// </summary>
+    enum State_1
+    {
+        А1=0,
+        OTHER
+    }
+    /// <summary>
+    /// Состояние в стеке по таблице 2
+    /// </summary>
+    enum State_2
+    {
+        IF=0,
+        OTHER
+    }
+}
